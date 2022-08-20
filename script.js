97fc1812f37437263cd68675ee690721986fa856
doydoo <copenhamdum@gmail.com>
2022-08-21 02:05:19 +0300

    Added default search results, article display on click, disposed of excceeding html and css

--- script.js
+++ script.js
@@ -1,73 +1,86 @@
-function init(){
+let DATA
+fetch('./data.json')
+  .then(resp => resp.json())
+  .then(data => DATA = data) //fetching dictionary data
 
-  let DATA
-  fetch('./data.json')
-    .then(resp => resp.json())
-    .then(data => DATA = data)
+function fillSearchResultsTable(){ //filling search-results table
+    let input = document.querySelector('input.search').value.toUpperCase()
+    let tbody = document.querySelector('table.search-results > tbody')
+    const rule = new RegExp('(?:^' + input + '[а-яА-Я]*)')
 
-  function fillTable(){
-      let input = document.querySelector('input.search').value.toUpperCase()
-      let tbody = document.querySelector('table.search-results > tbody')
-      const rule = new RegExp('(?:^' + input + '[а-яА-Я]*)')
+    if (input.length === 0){ //display default
+      for (let i = 0; i < DATA.length; i++){
+        let td = document.createElement('td')
+        td.textContent = DATA[i].title.toLowerCase()
+        let tr = document.createElement('tr')
+        tr.appendChild(td)
+        tbody.appendChild(tr)
+    }}
+    else{ //display successful search results
+      for (let i = 0; i < DATA.length; i++){
+        if ((DATA[i].title.match(rule))){
+              let td = document.createElement('td')
+              td.textContent = DATA[i].title.toLowerCase()
+              let tr = document.createElement('tr')
+              tr.appendChild(td)
+              tbody.appendChild(tr)
+          }
+      }
+    }
+    if (tbody.childNodes.length === 0){ //display default in case of unsuccessful search
+      for (let i = 0; i < DATA.length; i++){
+        let td = document.createElement('td')
+        td.textContent = DATA[i].title.toLowerCase()
+        let tr = document.createElement('tr')
+        tr.appendChild(td)
+        tbody.appendChild(tr)
+    }} 
+}
 
-      if (input.length === 0){
-        for (let i = 0; i < DATA.length; i++){
-          let td = document.createElement('td')
-          td.textContent = DATA[i].title.toLowerCase()
-          let tr = document.createElement('tr')
-          tr.appendChild(td)
-          tbody.appendChild(tr)
-      }}
-      else{
-        for (let i = 0; i < DATA.length; i++){
-          if ((DATA[i].title.match(rule))){
-                let td = document.createElement('td')
-                td.textContent = DATA[i].title.toLowerCase()
-                let tr = document.createElement('tr')
-                tr.appendChild(td)
-                tbody.appendChild(tr)
-            }
-        }
-      }   
-  }
+function refreshSearchResultsTable(){ //refreshing table on input and search queries
+    let tbody = document.querySelector('table.search-results > tbody')
+    tbody.replaceChildren()
+    fillSearchResultsTable()
+}
+
+function displayArticle(title){
+  const dictSlice = DATA.find(key => key.title === title)
+  let def = document.getElementById('def')
+  let content = document.getElementById('content')
+  let context = document.getElementById('context')
+  def.textContent = dictSlice.def
+  content.textContent = dictSlice.content
+  context.textContent = dictSlice.context
+}
+
+window.addEventListener('DOMContentLoaded', () => { //wrapping listeners
+
+  document.querySelector('tbody').addEventListener('load', loaded => {
+    if (loaded){
+      refreshSearchResultsTable()
+    }
+  })
   
-  function refreshTable(){
-      let tbody = document.querySelector('table.search-results > tbody')
-      tbody.replaceChildren()
-      fillTable()
-  }
-
   document.getElementById('search').addEventListener('keydown', key => {
       if (key.code === 'Enter'){
-        refreshTable()
+        refreshSearchResultsTable()
       }})
-
+  
   document.getElementById('search').addEventListener('input', change => {
     if (change){
-      refreshTable()
+      refreshSearchResultsTable()
     }})
-
+  
   document.querySelector('button.pretty-button').addEventListener('click', click => {
       if (click) {
-          refreshTable()
+          refreshSearchResultsTable()
       }
   })
 
-  function displayArticle(title){
-    const dictKeys = Object.values(DATA)
-    console.log(dictKeys)
-    const dictSlice = dictKeys.find(() => dictKeys.title === title)
-    let def = document.getElementById('def')
-    let content = document.getElementById('content')
-    let context = document.getElementById('context')
-    def.textContent = dictSlice.def
-    content.textContent = dictSlice.content
-    context.textContent = dictSlice.context
-  }
-  
-  document.querySelector('table.search-results').addEventListener('click', click => {
-    if (click.target && click.target.nodeName == "td" || "tr"){
-      // displayArticle(click.target.textContent.toUpperCase())
-      console.log(click.target.textContent)
+  document.querySelector('table.search-results > tbody').addEventListener('click', click => {
+    if (click.target && click.target.nodeName === "TD" || "TR"){
+      displayArticle(click.target.textContent.toUpperCase())
   }})
-}
+});
+
+window.onload = refreshSearchResultsTable
